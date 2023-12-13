31d5bed19002b48f1e276c2ca0b41f34da26b87f
motatan79 <m.pirela@gmail.com>
2023-12-13 12:47:02 -0300

    Primer Commit

---
+++ AppCoder/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+# Creación de clases 
+
+class Curso(models.Model):
+    
+    nombre = models.CharField(max_length=40)
+    camada = models.IntegerField()
+    
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+    
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+    profesion = models.CharField(max_length=30)
+    
+class Entregable(models.Model):
+    nombre = models.CharField(max_length=20)
+    fecha_entrega = models.DateField()
+    entregado = models.BooleanField()
+    
+    
